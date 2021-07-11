22ebbb43e438d3002a521a5e05865ed3b1f8964d
Ganesh Prajapati <ganeshprajapati@Ganeshs-MacBook-Pro.local>
2021-07-11 19:39:14 +0530

    math operation

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # Basic-Python
-# 1. print statement E.g : print('Hello, World')
-# 2. sum of two numbers
---
+++ math_operation.py
@@ -0,0 +1,17 @@
+num1 = 12
+num2 = 10
+
+# Sum
+print("sum :",num1+num2)
+
+# Subtraction
+print("Subtraction :", num1-num2)
+
+# Float Division
+print("Float Division :",num1/num2)
+
+# Flore Division
+print("Flore Division :",num1//num2)
+
+# Multiplication
+print("Multiplication :",num1*num2)
